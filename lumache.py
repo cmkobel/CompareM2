a6769555ef1e8e06b3d203ef1e7095b9d9f01458
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-06-01 17:48:04 +0200

    amend

--- lumache.py
+++ lumache.py
@@ -1,5 +1,15 @@
 """
 Lumache - Python library for cooks and food lovers.
+
+This is a Python docstring, we can use Markdown syntax here because
+our API documentation library understands it (mkdocstrings).
+
+    # Import lumache
+    import lumache
+
+    # Call its only function
+    get_random_ingredients(kind=["cheeses"])
+
 """
 
 __version__ = "0.1.0"
@@ -7,6 +17,7 @@ __version__ = "0.1.0"
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
+
     pass
 
 
