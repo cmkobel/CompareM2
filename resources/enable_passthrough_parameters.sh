ab5b68bf3c8079d1ee6a960f91094825cc5736e3
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-07-13 14:48:05 +0200

    passthrough enable

---
+++ resources/enable_passthrough_parameters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# In order to enable the passthrough parameters, hyphens must be allowed. With this oneliner, the regex pattern is modified from 
+# [a-zA-Z_]\w*$ to 
+# [a-zA-Z_][\w-]*\w$
+# in snakemake's parse_config().
+
+sed -i 's|valid = re.compile(r"\[a-zA-Z_\]\\w\*\$")|valid=re.compile(r"\[a-zA-Z_\]\[\\w-\]\*\\w\$")|g' $PREFIX/lib/python3.11/site-packages/snakemake/__init__.py
+
